851f78f03006b3136f8b08f0c45adb26e824fb7c
mce19 <maycolcastroesp19@gmail.com>
2023-03-23 19:08:39 -0600

    Inicio de mi blog

---
+++ CapaDatos/Data/BlogDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CapaDatos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.Data
+{
+    public class BlogDbContext : DbContext
+    {
+
+        public BlogDbContext(DbContextOptions opcion) : base(opcion)
+        { 
+        
+        }
+
+        //DBSet
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
